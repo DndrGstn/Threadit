56d0d08c47e8fe9929b36dc051bba02d01400dd9
DndrGstn <gustav.olov.haakansson@gmail.com>
2026-06-28 22:34:23 +0200

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- forumsite/settings.py
+++ forumsite/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "https://threadit-61b0f9e6eb22.herokuapp.com/",
+    "threadit-61b0f9e6eb22.herokuapp.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
